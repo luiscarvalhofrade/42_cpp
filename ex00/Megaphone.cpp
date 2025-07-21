60417021b0a270c0718fd325792669d10622aa38
luis frade <luis.carvalho.frade@gmail.com>
2025-07-21 14:12:19 -0300

    created ex00

---
+++ ex00/Megaphone.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    if (argc > 1)
+	{
+        std::cout << argv[1] << std::endl;
+    }
+	else
+	{
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    }
+    return 0;
+}
