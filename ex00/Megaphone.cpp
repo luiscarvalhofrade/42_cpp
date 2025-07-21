33ee7bb0985cde137ee24053c0b4e7b2004f7e9c
luis frade <luis.carvalho.frade@gmail.com>
2025-07-21 14:24:34 -0300

    fixed exercise, it was not printing all argvs and also did not make it uppercase letters

--- ex00/Megaphone.cpp
+++ ex00/Megaphone.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
+#include <cctype> // For std::toupper
 
 int main(int argc, char** argv)
 {
     if (argc > 1)
-	{
-        std::cout << argv[1] << std::endl;
+    {
+        for (int i = 1; i < argc; ++i)
+        {
+            for (char* c = argv[i]; *c; ++c)
+            {
+                std::cout << (char)std::toupper(*c);
+            }
+        }
+        std::cout << std::endl;
     }
-	else
-	{
+    else
+    {
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
     }
     return 0;
