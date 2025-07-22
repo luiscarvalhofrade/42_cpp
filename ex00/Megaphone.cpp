39e69a370e896145397660c9ffb8c64f4f4abaea
luis frade <luis.carvalho.frade@gmail.com>
2025-07-22 17:46:06 -0300

    started to implement Contact and PhoneBook files

--- ex00/Megaphone.cpp
+++ ex00/Megaphone.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
-#include <cctype> // For std::toupper
+#include <cctype>
 
 int main(int argc, char** argv)
 {
     if (argc > 1)
     {
-        for (int i = 1; i < argc; ++i)
+		for (int i = 1; i < argc; ++i)
         {
-            for (char* c = argv[i]; *c; ++c)
+			std::string str = argv[i];
+            for (int j = 0; str[j]; ++j)
             {
-                std::cout << (char)std::toupper(*c);
+                std::cout << (char)std::toupper(str[j]);
             }
         }
         std::cout << std::endl;
     }
     else
-    {
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-    }
     return (0);
 }
---
+++ ex01/Contact.hpp
@@ -0,0 +1,13 @@
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+
+class Contact {
+private:
+	char	first_name;
+	char	last_name;
+	int		phone;
+public:
+	Contact();
+};
+
+#endif
