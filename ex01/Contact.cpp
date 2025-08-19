256d1c545ee2a71b8e95af0c6cc7e875a62bc54d
luis frade <luis.carvalho.frade@gmail.com>
2025-08-19 15:59:24 -0300

    fixed Makefile, deleting a flag unnecessary and fixed a problem with set fn, ln and pn

--- ex01/Contact.cpp
+++ ex01/Contact.cpp
@@ -6,7 +6,7 @@
 /*   By: luide-ca <luide-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/23 10:43:37 by luide-ca          #+#    #+#             */
-/*   Updated: 2025/08/14 21:06:12 by luide-ca         ###   ########.fr       */
+/*   Updated: 2025/08/19 15:55:04 by luide-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,7 @@ std::string	Contact::setFirstName(void) {
 	std::string first_name;
 
 	std::cout << "Enter first name: ";
-	//std::cin.ignore();
+	std::cin.ignore();
 	std::getline(std::cin, first_name);
 	return (first_name);
 }
@@ -52,7 +52,6 @@ std::string	Contact::setLastName(void) {
 	std::string last_name;
 
 	std::cout << "Enter last name: ";
-	//std::cin.ignore();
 	std::getline(std::cin, last_name);
 	return (last_name);
 }
@@ -61,7 +60,6 @@ std::string	Contact::setPhoneNumber(void) {
 	std::string	phone_number;
 
 	std::cout << "Enter phone number: ";
-	//std::cin.ignore();
 	std::getline(std::cin, phone_number);
 	return (phone_number);
 }
