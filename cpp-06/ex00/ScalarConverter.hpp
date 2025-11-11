731f17342085f2a0275749868504483364fb31ce
Luis Frade <luis.carvalho.frade@gmail..com>
2025-11-11 09:09:01 -0300

    starting cpp 06 ex00

--- cpp-06/ex00/ScalarConverter.hpp
+++ cpp-06/ex00/ScalarConverter.hpp
@@ -1,12 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   ScalarConvert.hpp                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: luide-ca <luide-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/10 17:45:32 by luide-ca          #+#    #+#             */
-/*   Updated: 2025/11/10 17:45:34 by luide-ca         ###   ########.fr       */
+/*   Created: 2025/11/10 17:46:30 by luide-ca          #+#    #+#             */
+/*   Updated: 2025/11/10 17:54:07 by luide-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SCALAR_CONVERTER_HPP
+# define SCALAR_CONVERTER_HPP
+
+# include <iostream>
+
+class ScalarConverter {
+    private:
+
+    public:
+        static void convert(const std::string &input);
+};
+
+#endif
