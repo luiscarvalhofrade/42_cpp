08886674b23cba4ca2d5a744099d4aef9cc2ddd1
luis frade <luis.carvalho.frade@gmail.com>
2025-11-11 13:34:35 -0300

    wip ex00 cpp 06

--- cpp-06/ex00/ScalarConverter.cpp
+++ cpp-06/ex00/ScalarConverter.cpp
@@ -1,12 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ScalarConvert.cpp                                  :+:      :+:    :+:   */
+/*   ScalarConverter.cpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: luide-ca <luide-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/10 17:46:23 by luide-ca          #+#    #+#             */
-/*   Updated: 2025/11/10 17:46:24 by luide-ca         ###   ########.fr       */
+/*   Updated: 2025/11/11 11:21:40 by luide-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ScalarConverter.hpp"
+
+ScalarConverter::ScalarConverter(void) {
+
+}
+
+ScalarConverter::~ScalarConverter(void) {
+
+}
+
+ScalarConverter::ScalarConverter(const ScalarConverter& other) {
+
+}
+
+ScalarConverter& ScalarConverter::operator=(const ScalarConverter& other) {
+
+}
+
+static void ScalarConverter::convert(const std::string &input) {
+
+}
